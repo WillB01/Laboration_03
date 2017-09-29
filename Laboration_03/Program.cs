df582a5d7f33e25ff3678c2ff1d219c1e84b6ad6
WillB01 <wb.william@outlook.com>
2017-09-29 13:13:40 +0200

    Laboration 03 test

--- Laboration_03/Program.cs
+++ Laboration_03/Program.cs
@@ -11,23 +11,122 @@ namespace Laboration_03
     class Program
     {
         static List<string> adjektivList = new List<string>();
-        static List<string> AdverbList = new List<string>();
-        static List<string> InterjektionerList = new List<string>();
-        static List<string> KonjunktionerList = new List<string>();
+        static List<string> adverbList = new List<string>();
+        static List<string> interjektionerList = new List<string>();
+        static List<string> konjuktionerList = new List<string>();
+
+        static public int b;
+        static public Exception e;
 
 
         static void Main(string[] args)
         {
 
 
+
+
+
+           
             Adjektiv();
-            LoopsLists();
+            Adverb();
+            Interjektioner();
+            Konjuktioner();
+
+            LoopsLists(ChooseWordClass());
             
-            Kewl(ChooseWordClass());
-            
+
+
+
+
+
+
+
             Console.ReadLine();
         }
 
+
+        static void LoopsLists(int k)
+        {
+
+            switch (k)
+            {
+                case 1:
+                    foreach (var item in adjektivList)
+                    {
+
+                        Console.Write(adjektivList.IndexOf(item) + 1);
+
+                        Console.WriteLine(": {0}", item);
+                    }
+                    break;
+                case 2:
+                    foreach (var item in adverbList)
+                    {
+
+                        Console.Write(adverbList.IndexOf(item) + 1);
+
+                        Console.WriteLine(": {0}", item);
+                    }
+                    break;
+                case 3:
+                    foreach (var item in interjektionerList)
+                    {
+
+                        Console.Write(interjektionerList.IndexOf(item) + 1);
+
+                        Console.WriteLine(": {0}", item);
+                    }
+                    break;
+                case 4:
+                    foreach (var item in konjuktionerList)
+                    {
+
+                        Console.Write(konjuktionerList.IndexOf(item) + 1);
+
+                        Console.WriteLine(": {0}", item);
+                    }
+                    break;
+
+            }   
+        }
+
+
+
+        static int ChooseWordClass()
+        {
+            bool loopInput = true;
+            while (loopInput)
+            {
+                Console.Write("Välj en ordklass:\n" +
+                "Tryck: 1 - Adjektiv\n" +
+                "Tryck: 2 - Adverb\n" +
+                "Tryck: 3 - Interjektioner\n" +
+                "Tryck: 4 - Konjuktioner\n" +
+                "Svar: ");
+           
+                try
+                {
+                    b = int.Parse(Console.ReadLine());
+
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+               if (b == 1 || b == 2 || b == 3 || b == 4 )
+                {
+                    loopInput = false;
+                }
+            
+            }
+            return b;
+
+
+
+
+        }
+
+       
         static void Adjektiv()
         {
             adjektivList.Add("Röd");
@@ -40,82 +139,59 @@ namespace Laboration_03
             adjektivList.Add("Glad");
             adjektivList.Add("Arg");
             adjektivList.Add("Lesden");
-            
+
         }
 
-        static void Addverb()
+        static void Adverb()
         {
-            adjektivList.Add("Röd");
-            adjektivList.Add("Gul");
-            adjektivList.Add("Svart");
-            adjektivList.Add("Stark");
-            adjektivList.Add("Svag");
-            adjektivList.Add("Stor");
-            adjektivList.Add("Ung");
-            adjektivList.Add("Glad");
-            adjektivList.Add("Arg");
-            adjektivList.Add("Lesden");
+            adverbList.Add("Fort");
+            adverbList.Add("Ovanligt");
+            adverbList.Add("Mycket");
+            adverbList.Add("När");
+            adverbList.Add("Där");
+            adverbList.Add("Dit");
+            adverbList.Add("Inte");
+            adverbList.Add("Ner");
+            adverbList.Add("Upp");
+            adverbList.Add("Efter");
+
+
 
         }
 
         static void Interjektioner()
         {
-            adjektivList.Add("Röd");
-            adjektivList.Add("Gul");
-            adjektivList.Add("Svart");
-            adjektivList.Add("Stark");
-            adjektivList.Add("Svag");
-            adjektivList.Add("Stor");
-            adjektivList.Add("Ung");
-            adjektivList.Add("Glad");
-            adjektivList.Add("Arg");
-            adjektivList.Add("Lesden");
+            interjektionerList.Add("Hej");
+            interjektionerList.Add("Usch");
+            interjektionerList.Add("Tick-Tack");
+            interjektionerList.Add("Boom");
+            interjektionerList.Add("God natt");
+            interjektionerList.Add("Skål");
+            interjektionerList.Add("Bravo");
+            interjektionerList.Add("Ja");
+            interjektionerList.Add("Pang");
+            interjektionerList.Add("Fy");
+
+
 
         }
 
         static void Konjuktioner()
         {
-            adjektivList.Add("Röd");
-            adjektivList.Add("Gul");
-            adjektivList.Add("Svart");
-            adjektivList.Add("Stark");
-            adjektivList.Add("Svag");
-            adjektivList.Add("Stor");
-            adjektivList.Add("Ung");
-            adjektivList.Add("Glad");
-            adjektivList.Add("Arg");
-            adjektivList.Add("Lesden");
+            konjuktionerList.Add("Och");
+            konjuktionerList.Add("Eller");
+            konjuktionerList.Add("Utan");
+            konjuktionerList.Add("Men");
+            konjuktionerList.Add("För");
+            konjuktionerList.Add("Fast");
+            konjuktionerList.Add("Varken");
+            konjuktionerList.Add("Att");
+            konjuktionerList.Add("När");
+            konjuktionerList.Add("Som");
+
+
 
         }
-
-        static void LoopsLists()
-        {
-            foreach (var item in adjektivList)
-            {
-
-                Console.Write(adjektivList.IndexOf(item) + 1);
-
-                Console.WriteLine(": {0}", item);
-            }
-
-        }
-
-        static int ChooseWordClass()
-        {
-            Console.WriteLine("Välj list 1 eller 2");
-            int b = int.Parse(Console.ReadLine());
-            return b;
-        }
-
-        static void Kewl(int chooseWordClass)
-        {
-            if(chooseWordClass == 1)
-            {
-                LoopsLists();
-            }
-        }
-
-
 
 
 
